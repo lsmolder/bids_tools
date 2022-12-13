b9118934a2fa4fa3cfecc8ef5d5bb4c36b6fe0f5
Amr Eed <amr.kamel@uv.es>
2022-12-13 10:47:56 -0500

    add todo list

---
+++ dcm2bids.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# take the animal number, dicom directory task and convert them to bids format
+
+#
+# TODO: take in the name
+# TODO: the task
+# TODO: cut the videos to match the name
+# TODO: phase or magnitude
+# TODO: normal phase or reverse phase
+# TODO: how the dicom should be organized
