3673a9a7e2f7bed3a86b0d748c5acdfe6dbc7519
Amr Eed <amr.kamel@uv.es>
2022-12-15 11:16:38 -0500

    recognize normal and reverse phase

--- heuristic_9T.py
+++ heuristic_9T.py
@@ -10,7 +10,7 @@ try:
          curr_dir = glob.glob("*")
          os.chdir(curr_dir[-1])
 
-    # once you face dicoms, go up one level (that's where all data is)     
+    # once you face dicoms, go up one level (that's where all data is)
 except NotADirectoryError:
     os.chdir(os.path.dirname(os.getcwd()))
 
@@ -30,13 +30,16 @@ def infotodict(seqinfo):
     subindex: sub index within group
     """
     t2w = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_run-00{item:01d}_T2w')
-    func_rest_magnitude = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-rest_run-00{item:01d}_bold_magnitude')
-    func_rest_phase = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-rest_run-00{item:01d}_bold_phase')
+    func_rest_magnitude_R = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-rest_run-00{item:01d}_bold_magnitude_R')
+    func_rest_phase_R = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-rest_run-00{item:01d}_bold_phase_R')
 
-    info = {t2w: [], func_rest_magnitude: [], func_rest_phase: []}
-    
+    func_rest_magnitude_RV = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-rest_run-00{item:01d}_bold_magnitude_RV')
+    func_rest_phase_RV = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-rest_run-00{item:01d}_bold_phase_RV')
 
-    # extract the digits of the name to seperate 
+    info = {t2w: [], func_rest_magnitude_R: [], func_rest_phase_R: [], func_rest_magnitude_RV: [], func_rest_phase_RV: []}
+
+
+    # extract the digits of the name to seperate
     # you can even add the videos
     # magnitude data has name: 170001, phase: 170002
     # reverse phase and normal phase
@@ -45,10 +48,17 @@ def infotodict(seqinfo):
     for idx, s in enumerate(seqinfo):
         if ('T2_TurboRARE' in s.protocol_name):
             info[t2w].append(s.series_id)
-
+        # if the name does not contain "_RV_" then it is a normal phase
         if ('T2star_FID_EPI_sat' in s.protocol_name) and (int(s.dcm_dir_name[-1]) == 1):
-            info[func_rest_magnitude].append(s.series_id)
+            info[func_rest_magnitude_R].append(s.series_id)
 
         if ('T2star_FID_EPI_sat' in s.protocol_name) and (int(s.dcm_dir_name[-1]) == 2):
-            info[func_rest_phase].append(s.series_id)
+            info[func_rest_phase_R].append(s.series_id)
+
+        # if the name contains "_RV_" then it is a reveresed phase
+        if ('T2star_FID_EPI_sat' in s.protocol_name) and (int(s.dcm_dir_name[-1]) == 1) and ("_RV_" in s.series_description):
+            info[func_rest_magnitude_RV].append(s.series_id)
+
+        if ('T2star_FID_EPI_sat' in s.protocol_name) and (int(s.dcm_dir_name[-1]) == 2) and ("_RV_" in s.series_description):
+            info[func_rest_phase_RV].append(s.series_id)
     return info
