c999f150cbe6c6773b179734f973eb7e99d1cf30
Amr Eed <amr.kamel@uv.es>
2023-03-27 10:38:54 -0400

    correct some errors

--- batch_processing.sh
+++ batch_processing.sh
@@ -6,15 +6,15 @@
 # 04_393073_M^^^^ 06_393073_M^^^^ 41_393072_F^^^^ 44_393072_F^^^^
 # 05_393073_M^^^^ 07_393073_M^^^^ 43_393072_F^^^^
 for folder in *; do
-  name=$(echo ${name} | sed 's/\^\^\^\^//g')
-  echo ${name}
+  name=$(echo "${folder}" | sed 's/\^\^\^\^//g')
+  echo "${name}"
   heudiconv \
-    --files $folder \
-    --outdir /Users/aeed/Documents/Work/awakeMice_Grandjean/bids \
-    --subjects $(zeropad $name 12) \
-    --ses s1 \
-    --heuristic /Users/aeed/Documents/Work/bids_tools/bids_tools/heuristic_9T_rest_awake.py \
-    --converter dcm2niix \
-    --bids \
-    --overwrite
+  --files "${PWD}"/"${folder}" \
+  --outdir /Users/aeed/Documents/Work/awakeMice_Grandjean/bids \
+  --subjects $(zeropad "$name" 12) \
+  --ses s1 \
+  --heuristic /Users/aeed/Documents/Work/bids_tools/bids_tools/heuristic_9T_rest_awake.py \
+  --converter dcm2niix \
+  --bids \
+  --overwrite
 done
