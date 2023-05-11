f22d2c41b446a6322dbc49a196236bc792f4b39a
Amr Eed <amr.kamel@uv.es>
2023-05-11 10:28:43 -0400

    more TODO stuff

--- dcm2bids.sh
+++ dcm2bids.sh
@@ -15,19 +15,22 @@
 # TODO: take the videos if the task matches whatever you define as video task
 # TODO: add extra desription to the script does => done
 # TODO: test on Arthur's Brown data as well => done
-# TODO: delete all unnecessary dirs => done (delet the DICOM dir)
-# TODOL multiple sessions, maybe add an extra flag or make conditional if subject existed => done (works automatically)
+# TODO: delete all unnecessary dirs => done (delete the DICOM dir)
+# TODO: multiple sessions, maybe add an extra flag or make conditional if subject existed => done (works automatically)
 # TODO: take in session number as well => done
 # TODO: accept field maps as well => done
 # TODO: add requirements file
-# TODO: check that the runs are organized correctly
+# TODO: check that the runs are organized correctly => done
 # TODO: extract useful info from the excel sheet
 # TODO: extract the subject's name and sessions from the zip or json
 # TODO: check if the json files have acquistion date and start and duration
-
+# TODO: verify no of output files against no of input files
+# TODO: verify no of output files against no of dcm2niix files
+# TODO: write a script to change dicom fodlers if the names of mag and phase like 430003 430004, not 001 and 002
+# TODO: write a script to count the number of output from bids and input dcms
 
 function Usage {
-    cat <<USAGE
+  cat <<USAGE
 
 Usage:
 
@@ -65,47 +68,43 @@ $(basename "$0") \
 -f heuristic_9T.py
 
 USAGE
-    exit 1
+  exit 1
 }
 
+if [[ "$1" == "-h" || $# -eq 0 ]]; then
+  Usage >&2
+fi
 
-if [[ "$1" == "-h" || $# -eq 0 ]];
-  then
-    Usage >&2
-  fi
-
-while getopts "h:z:i:s:t:d:f:" OPT
-  do
+while getopts "h:z:i:s:t:d:f:" OPT; do
   case $OPT in
-      h) #help
-   Usage
-   exit 0
-   ;;
-      z)  # zipped dcm file
-   DICOM_ZIP=$OPTARG
-   ;;
-      i)  # animal id
-   ANIMAL_ID=$OPTARG
-   ;;
-   s)  # animal id
-   SESSION_NO=$OPTARG
-   ;;
-      t)  # task type
-   TASK=$OPTARG
-   ;;
-      d)  # output bids dir
-   BIDS_DIR=$OPTARG
-   ;;
-     f)  # output bids dir
-   HEURISTIC=$OPTARG
-   ;;
-     \?) # getopts issues an error message
-   echo "$Usage" >&2
-   exit 1
-   ;;
+  h) #help
+    Usage
+    exit 0
+    ;;
+  z) # zipped dcm file
+    DICOM_ZIP=$OPTARG
+    ;;
+  i) # animal id
+    ANIMAL_ID=$OPTARG
+    ;;
+  s) # animal id
+    SESSION_NO=$OPTARG
+    ;;
+  t) # task type
+    TASK=$OPTARG
+    ;;
+  d) # output bids dir
+    BIDS_DIR=$OPTARG
+    ;;
+  f) # output bids dir
+    HEURISTIC=$OPTARG
+    ;;
+  \?) # getopts issues an error message
+    echo "$Usage" >&2
+    exit 1
+    ;;
   esac
 done
-
 
 # decompress the dcm file in the zipped file dir (it overwrites if exists automatically)
 ZIP_DIR=$(dirname "${DICOM_ZIP}") # get the parent dir
@@ -117,15 +116,14 @@ unzip -o "${DICOM_ZIP}" -d "${ZIP_DIR}"/"${ANIMAL_ID}"_dcm
 # the heudiconv removes the underscore, so a zeropad will make it easier to identify
 # aka 1st 3 places are ear number, next 6 digits are the cage number, anf the letter is the gender
 heudiconv \
---files "${ZIP_DIR}"/"${ANIMAL_ID}"_dcm   \
---outdir "${BIDS_DIR}"  \
---subjects $(zeropad "${ANIMAL_ID}" 12)  \
---ses "${SESSION_NO}" \
---heuristic "${HEURISTIC}"  \
---converter dcm2niix \
---bids \
---overwrite
-
+  --files "${ZIP_DIR}"/"${ANIMAL_ID}"_dcm \
+  --outdir "${BIDS_DIR}" \
+  --subjects $(zeropad "${ANIMAL_ID}" 12) \
+  --ses "${SESSION_NO}" \
+  --heuristic "${HEURISTIC}" \
+  --converter dcm2niix \
+  --bids \
+  --overwrite
 
 # delete the dicom folder
 rm -rf "${ZIP_DIR}"/"${ANIMAL_ID}"_dcm
