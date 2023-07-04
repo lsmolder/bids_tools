e8d618042a984b3c7de896cd87feb20a3a2fea65
Amr Eed <amr.kamel@uv.es>
2023-07-04 19:25:28 -0400

    modify heuristic to work with v0.13

--- heuristic_9T_Brown^TBI2AD.py
+++ heuristic_9T_Brown^TBI2AD.py
@@ -16,7 +16,7 @@ except NotADirectoryError:
 
 
 # =============================================================================================================================
-def create_key(template, outtype='nii.gz', annotation_classes=None):
+def create_key(template, outtype=('nii.gz',), annotation_classes=None):
     if template is None or not template:
         raise ValueError('Template must be a valid format string')
     return template, outtype, annotation_classes
--- heuristic_9T_rest_awake.py
+++ heuristic_9T_rest_awake.py
@@ -28,7 +28,7 @@ except NotADirectoryError:
 
 
 # ======================================================================================================================
-def create_key(template, outtype='nii.gz', annotation_classes=None):
+def create_key(template, outtype=('nii.gz',), annotation_classes=None):
     if template is None or not template:
         raise ValueError('Template must be a valid format string')
 
